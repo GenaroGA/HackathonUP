0b462ea18fe44879f7a73f114b94f775c5421083
iOS Lab <iOS Lab@Macbook-Air-21.local>
2023-04-29 16:56:48 -0600

    Creación de pantalla principal, LogInView y RegisterView

--- iShare/iShare/ContentView.swift
+++ iShare/iShare/ContentView.swift
@@ -7,16 +7,56 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack
+struct ContentView: View
+{
+    var body: some View
+    {
+        NavigationView()
         {
-            Text("App iShare")
+            ZStack
+            {
+                Color(.blue)
+                    .ignoresSafeArea()
+                VStack
+                {
+                    Circle()
+                        .frame(width: 150, height: 150)
+                    Text("App iShare")
+                        .font(.largeTitle)
+                    HStack
+                    {
+                        Spacer()
+                        NavigationLink(destination: LoginView()) {
+                            Text("Login")
+                                .foregroundColor(Color.white)
+                                .frame(width: 70, height: 30)
+                                .cornerRadius(15)
+                                .background(Color.blue)
+                        }
+                        
+                        NavigationLink(destination: RegisterView()) {
+                            Text("Register")
+                                .foregroundColor(Color.white)
+                                .frame(width: 70, height: 30)
+                                .cornerRadius(15)
+                                .background(Color.blue)
+                        }
+                        Spacer()
+                    }
+                    
+                    
+                        
+
+                }
+            }
+            .foregroundColor(.white)
         }
-
-
+        
     }
+
+
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ iShare/iShare/MainMenuView.swift
@@ -0,0 +1,20 @@
+//
+//  MainMenuView.swift
+//  iShare
+//
+//  Created by iOS Lab on 29/04/23.
+//
+
+import SwiftUI
+
+struct MainMenuView: View {
+    var body: some View {
+        Text("Menu!!")
+    }
+}
+
+struct MainMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainMenuView()
+    }
+}
